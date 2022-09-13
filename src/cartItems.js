f234bf0c97575dd5b9debd9aff03936146a73fd2
Bach Hai <haibach1299@gmail.com>
2022-09-13 21:46:27 +0700

    'finished'

---
+++ src/cartItems.js
@@ -0,0 +1,32 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Iphone 14 Pro Max',
+    price: '1799.99',
+    img: 'https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/iPhone_14_Pro_Max-Pur1.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    title: 'Galaxy s22 ultra',
+    price: '1499.99',
+    img: 'https://hoanghamobile.com/Uploads/2022/02/09/image-removebg-preview-19.png',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Galaxy Z-Fold 4',
+    price: '2299.99',
+    img: 'https://cdn2.cellphones.com.vn/358x/media/catalog/product/s/a/samsung_galaxy_z_fold_4-4.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Sony X1 Mark-IV',
+    price: '1399.99 ',
+    img: 'https://product.hstatic.net/1000370129/product/sony-xperia-1-iv-white-digiphone_2e4d7f92adad4b56b68c99668038e3bd_grande.jpg',
+    amount: 1,
+  },
+];
+
+export default cartItems;
